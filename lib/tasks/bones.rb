06a1bddb4f9ad2afb690233d4891903f93461004
rpheath <rpheath@gmail.com>
2008-08-27 11:38:20 -0400

    added rake task to execute specs (todo: consider location of Rakefile)

--- lib/tasks/bones.rb
+++ lib/tasks/bones.rb
@@ -20,6 +20,25 @@ namespace :cache do
   end
 end
 
+namespace :specs do
+  task :run do
+    specs = [
+      'bones_spec.rb',
+      'helpers/core_helper_spec.rb'
+    ]
+  
+    specs.each do |f|
+      puts "\n** Executing #{f}"
+      # TODO
+      # fix so that specs can be
+      # executed from within either
+      # directory: lib/ or bones/
+      # (currently assumes lib/)
+      system "spec ../spec/#{f}"
+    end
+  end
+end
+
 task :clean do
   # TODO
   # ARGV.shift
